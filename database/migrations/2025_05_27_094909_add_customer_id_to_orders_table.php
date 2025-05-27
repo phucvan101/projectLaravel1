0c32ea5fb1e8cd1a436250bd986d5209847272cf
phucvan101 <111259337+phucvan101@users.noreply.github.com>
2025-05-27 10:26:54 +0700

    sửa lại đặt hàng

---
+++ database/migrations/2025_05_27_094909_add_customer_id_to_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('customer_id')->nullable()->after('id');
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('set null'); // nếu user bị xóa thì customer_id sẽ được set về null
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+    }
+};
